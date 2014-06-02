15aac69eeb809814daa3c5f4650054d700fd61b0
kpe <kpe.git@gmailbox.org>
2014-06-02 18:07:51 +0200

    simlep mathjax plugin through interception

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-tw5-mathjax-plugin
-==================
+#[tw5-mathjax-plugin](https://kpe.github.io/tw5-mathjax-plugin/)
 
-TW5 plugin adding support for LaTeX through MathJax
+
+TW5 plugin adding support for LaTeX through MathJax.
+
+#Usage
+
+Try something like this in a tiddler (or it's title):
+```
+Some inline math: $\sum_{i=1}^{100}i = 5050$, as:
+$$
+  \sum_{i=1}^{100} = (1+100)\frac{100}{2}
+$$
+```
+
---
+++ mathjax.js
@@ -0,0 +1,43 @@
+/*\
+title: $:/plugins/kpe/mathjax/mathjax.js
+type: application/javascript
+module-type: startup
+
+Renders MathJax in tiddlers
+
+\*/
+(function() {
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+
+    var SetWidget = require("$:/core/modules/widgets/set.js").set;
+    var superRender = SetWidget.prototype.render;
+    var superRefresh = SetWidget.prototype.refresh;
+
+    function doMathJax(el) {
+       if(typeof MathJax != 'undefined') {
+            // MathJax.Hub.TypeSet(this.parentDomNode);
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+        }
+    }
+
+    // intercept render
+    SetWidget.prototype.render = function(parent,nextSibling) {
+        superRender.call(this, parent, nextSibling);
+        if(this.setName == 'storyTiddler') {
+//            console.log('doing mathjax on:',this.parentDomNode);
+            doMathJax(this.parentDomNode);
+        }
+    };
+    // intercept refresh
+    SetWidget.prototype.refresh = function(changedTiddlers) {
+        superRefresh.call(this, changedTiddlers);
+        if(this.setName == 'storyTiddler') {
+//            console.log('refreshing mathjax on:',this.parentDomNode);
+            doMathJax(this.parentDomNode);
+        }
+    };
+
+})();
