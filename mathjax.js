699c6ba1b2e26592c6185d97f357c6982e92c38c
kpe <kpe.git@gmailbox.org>
2014-06-02 18:30:03 +0200

    fix: its a startup module

--- mathjax.js
+++ mathjax.js
@@ -12,32 +12,40 @@ Renders MathJax in tiddlers
     /*global $tw: false */
     "use strict";
 
-    var SetWidget = require("$:/core/modules/widgets/set.js").set;
-    var superRender = SetWidget.prototype.render;
-    var superRefresh = SetWidget.prototype.refresh;
+    exports.startup = function(){
+        var SetWidget = require("$:/core/modules/widgets/set.js").set;
+        var superRender = SetWidget.prototype.render;
+        var superRefresh = SetWidget.prototype.refresh;
 
-    function doMathJax(el) {
-       if(typeof MathJax != 'undefined') {
-            // MathJax.Hub.TypeSet(this.parentDomNode);
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+        function doMathJax(el) {
+           if(typeof MathJax != 'undefined') {
+                // MathJax.Hub.TypeSet(this.parentDomNode);
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+            }
         }
-    }
 
-    // intercept render
-    SetWidget.prototype.render = function(parent,nextSibling) {
-        superRender.call(this, parent, nextSibling);
-        if(this.setName == 'storyTiddler') {
-//            console.log('doing mathjax on:',this.parentDomNode);
-            doMathJax(this.parentDomNode);
-        }
+        // intercept render
+        SetWidget.prototype.render = function(parent,nextSibling) {
+            superRender.call(this, parent, nextSibling);
+            if(this.setName == 'storyTiddler') {
+    //            console.log('doing mathjax on:',this.parentDomNode);
+                doMathJax(this.parentDomNode);
+            }
+        };
+        // intercept refresh
+        SetWidget.prototype.refresh = function(changedTiddlers) {
+            superRefresh.call(this, changedTiddlers);
+            if(this.setName == 'storyTiddler') {
+    //            console.log('refreshing mathjax on:',this.parentDomNode);
+                doMathJax(this.parentDomNode);
+            }
+        };
+
     };
-    // intercept refresh
-    SetWidget.prototype.refresh = function(changedTiddlers) {
-        superRefresh.call(this, changedTiddlers);
-        if(this.setName == 'storyTiddler') {
-//            console.log('refreshing mathjax on:',this.parentDomNode);
-            doMathJax(this.parentDomNode);
-        }
-    };
+
+    exports.name = "mathjax";
+    exports.platforms = ["browser"];
+    exports.after = ["startup"];
+    exports.synchronous = false;
 
 })();
